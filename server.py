f489b0b8d9f870559ed28e1bde5a4ae188b8e918
dfgomezmstarter <42325655+dfgomezmstarter@users.noreply.github.com>
2020-10-24 21:44:19 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Oct 18 11:11:41 2020
-
 @author: Daniel Felipe Gomez Martinez, Cesar Andres Garcia Posada
 """
 
-#Import libraries for networking communication
+# Import libraries for networking communication
 import _thread
 import socket
 import constants
@@ -14,14 +13,15 @@ import sys
 import shutil
 import time
 
-# import thread module 
-import threading 
+# import thread module
+import threading
 
-print_lock = threading.Lock() 
+print_lock = threading.Lock()
 bucket_route = ""
 
-#Create a socket
+# Create a socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 def threaded(client_connection, client_address, route):
     while True:
@@ -31,7 +31,7 @@ def threaded(client_connection, client_address, route):
         command = remote_command[0]
         print(f'Data received from: {client_address[0]}:{client_address[1]}')
         print(command)
-        if(command == constants.QUIT):
+        if (command == constants.QUIT):
             response = '200 BYE\n'
             client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
             print(f'Now, client {client_address[0]}:{client_address[1]} is disconnected.')
@@ -41,33 +41,33 @@ def threaded(client_connection, client_address, route):
             client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
             response = constants.HELP_ARRAY2
             client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
-        elif(command == constants.CREATE_B):
+        elif (command == constants.CREATE_B):
             try:
                 name = remote_command[1]
                 name = "\\" + name
-                os.mkdir(route+name)
+                os.mkdir(route + name)
                 response = '300 BCS\n'
-            except :
+            except:
                 print("Fatal error bucket doesnt have name")
                 response = '301 BCF\n'
             client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
-        elif(command == constants.LIST_B):
+        elif (command == constants.LIST_B):
             lista = os.listdir(route)
             lista.append("400 LS")
             response = str(lista)
             client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
-        elif(command == constants.DELETE_B):
+        elif (command == constants.DELETE_B):
             try:
                 name = remote_command[1]
                 name = "\\" + name
-                shutil.rmtree(route+name)
+                shutil.rmtree(route + name)
                 response = '600 DBS\n'
                 client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
-            except :
+            except:
                 print("Fatal error delete a bucket")
                 response = '601 DBF\n'
                 client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
-        elif(command == constants.UPLOAD):
+        elif (command == constants.UPLOAD):
             destination = remote_command[1]
             destination = "\\" + destination
             name = remote_command[2]
@@ -92,8 +92,8 @@ def threaded(client_connection, client_address, route):
                 print("ERROR: " + str(e))
                 response = '701 FUBF\n'
                 client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
-        elif(command == constants.LIST_F):
-            dic = {} 
+        elif (command == constants.LIST_F):
+            dic = {}
             for dirName, subdirList, fileList in os.walk(route):
                 array = []
                 for fname in fileList:
@@ -104,22 +104,23 @@ def threaded(client_connection, client_address, route):
             client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
         elif (command == constants.DOWNLOAD):
             origin_bucket = remote_command[1]
-            file_name = remote_command[2]
-            origin_file_path = route + '\\' + origin_bucket + '\\' + file_name
+            name = remote_command[2]
+            origin_file_path = route + '\\' + origin_bucket + '\\' + name
             origin_file_path = origin_file_path.replace("\\", '/')
             try:
-                file_size = str(os.path.getsize(origin_file_path))
-                client_connection.send(bytes(file_size, constants.ENCODING_FORMAT))
+                size = str(os.path.getsize(origin_file_path))
+                client_connection.send(bytes(size, constants.ENCODING_FORMAT))
+                time.sleep(1)
                 file = open(origin_file_path, 'rb')
                 line = file.read(constants.RECV_BUFFER_SIZE)
                 while (line):
                     client_connection.send(line)
                     line = file.read(constants.RECV_BUFFER_SIZE)
                 file.close()
-                response = '900 DFS\n'
+                response = '900 FDBS\n'
             except BaseException as e:
                 print("ERROR: " + str(e))
-                response = '901 DFF\n'
+                response = '901 FDBF\n'
             client_connection.sendall(response.encode(constants.ENCODING_FORMAT))
         elif (command == constants.DELETE_F):
             try:
@@ -142,14 +143,15 @@ def threaded(client_connection, client_address, route):
         pass
     client_connection.close()
 
-#Main function
+
+# Main function
 def main():
     try:
         bucket_route = sys.argv[1]
     except:
         print("Invalid route")
         sys.exit(1)
-    print('*'*50)
+    print('*' * 50)
     print('Server is running...')
     print('IP addr: ', constants.SERVER_ADDRESS)
     print('Port: ', constants.PORT)
@@ -158,15 +160,16 @@ def main():
     server_socket.bind(tuple_connection)
     server_socket.listen(constants.BACKLOG)
     print('Socket is listening...', server_socket.getsockname())
-    
-    #Loop for waiting new conection
+
+    # Loop for waiting new conection
     while True:
         client_connection, client_address = server_socket.accept()
-         
-        print(f'New incoming connection is accepted. Remote IP address: {client_address[0]}')  
-        # Start a new thread and return its identifier 
-        _thread.start_new_thread(threaded, (client_connection,client_address, bucket_route)) 
+
+        print(f'New incoming connection is accepted. Remote IP address: {client_address[0]}')
+        # Start a new thread and return its identifier
+        _thread.start_new_thread(threaded, (client_connection, client_address, bucket_route))
     server_socket.close()
+
 
 if __name__ == '__main__':
     main()
